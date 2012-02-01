88a3605bf22e1667ba92b16380c5e6619c327aa4
unknown <GM@venkatesh.(none)>
2012-02-01 17:38:10 +0530

    flash added

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -4,6 +4,7 @@ class AuthenticationsController < ApplicationController
   end
   
   def create
+    flash[:notice] = "Signed in from create."
     omniauth = request.env["omniauth.auth"]
     authentication = Authentication.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
     if authentication
